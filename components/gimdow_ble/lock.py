467b9705f06d6f1280bacb3dca3b171ed35733ef
Valeriy <81035252+prokudin07@users.noreply.github.com>
2026-09-21 22:30:08 +0300

    Add required lock model configuration

--- components/gimdow_ble/lock.py
+++ components/gimdow_ble/lock.py
@@ -17,27 +17,49 @@ GimdowBLELock = gimdow_ble_ns.class_(
     cg.Component,
 )
 
+CONF_MODEL = "model"
 CONF_LOCAL_KEY = "local_key"
 CONF_UUID = "uuid"
 CONF_DEVICE_ID = "tuya_device_id"
+CONF_BLE_UNLOCK_CHECK = "ble_unlock_check"
 CONF_STATE_SENSOR = "state_sensor"
 
-CONFIG_SCHEMA = (
+MODEL_A1_PRO_MAX = "a1_pro_max"
+MODEL_A1_ULTRA = "a1_ultra"
+
+MODEL_IDS = {
+    MODEL_A1_PRO_MAX: 0,
+    MODEL_A1_ULTRA: 1,
+}
+
+
+def _validate_model_options(config):
+    if config[CONF_MODEL] == MODEL_A1_ULTRA and CONF_BLE_UNLOCK_CHECK not in config:
+        raise cv.Invalid(
+            "model 'a1_ultra' requires the device-specific 'ble_unlock_check' value"
+        )
+    return config
+
+
+CONFIG_SCHEMA = cv.All(
     lock.lock_schema(GimdowBLELock)
     .extend(
         {
             cv.GenerateID(CONF_BLE_CLIENT_ID): cv.use_id(
                 ble_client.BLEClient
             ),
+            cv.Required(CONF_MODEL): cv.one_of(*MODEL_IDS, lower=True),
             cv.Required(CONF_LOCAL_KEY): cv.string,
             cv.Required(CONF_UUID): cv.string,
             cv.Required(CONF_DEVICE_ID): cv.string,
+            cv.Optional(CONF_BLE_UNLOCK_CHECK): cv.string,
             cv.Optional(CONF_STATE_SENSOR): cv.use_id(
                 binary_sensor.BinarySensor
             ),
         }
     )
-    .extend(cv.COMPONENT_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA),
+    _validate_model_options,
 )
 
 
@@ -52,9 +74,13 @@ async def to_code(config):
     cg.add(parent.register_ble_node(var))
     cg.add(var.set_parent(parent))
 
+    cg.add(var.set_model(MODEL_IDS[config[CONF_MODEL]]))
     cg.add(var.set_local_key(config[CONF_LOCAL_KEY]))
     cg.add(var.set_uuid(config[CONF_UUID]))
     cg.add(var.set_device_id(config[CONF_DEVICE_ID]))
+
+    if CONF_BLE_UNLOCK_CHECK in config:
+        cg.add(var.set_ble_unlock_check(config[CONF_BLE_UNLOCK_CHECK]))
 
     if CONF_STATE_SENSOR in config:
         sensor = await cg.get_variable(config[CONF_STATE_SENSOR])
